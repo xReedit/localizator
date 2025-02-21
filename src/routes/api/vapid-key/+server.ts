f47157b6e583cf0d7d0ba59f56ef4af6daf9b2f9
xReedit <papaya.restobar@gmail.com>
2025-02-21 02:36:25 -0500

    a4

--- src/routes/api/vapid-key/+server.ts
+++ src/routes/api/vapid-key/+server.ts
@@ -1,7 +1,7 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
-import { PUBLIC_VAPID_KEY } from '$env/static/public';
+import { env } from '$env/dynamic/public';
 
 export const GET: RequestHandler = async () => {
-    return json({ publicKey: PUBLIC_VAPID_KEY });
+    return json({ publicKey: env.PUBLIC_VAPID_PUBLIC_KEY });
 };
